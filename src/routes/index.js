500a2586150e97fb69b96eac149ec3655061c932
djjwenger <djjwenger@hotmail.com>
2022-11-14 19:22:29 -0500

    add cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const books = require("./books");
 const movies = require("./movies");
 const reviews = require("./reviews");
 const users = require("./users");
+const openCors = require("../middleware/cors");
 
+routes.use(openCors);
 routes.use("/", require("./swagger"));
 routes.use("/books", books);
 routes.use("/movies", movies);
