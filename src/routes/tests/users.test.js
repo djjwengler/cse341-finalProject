e94ed754d27301c25fb402d772e466317bbfddf1
djjwenger <djjwenger@hotmail.com>
2022-11-28 13:51:40 -0500

    add non auth0 user route tests

--- src/routes/tests/users.test.js
+++ src/routes/tests/users.test.js
@@ -1,65 +1,33 @@
-describe("Sanity test", () => {
-  test("1 should equal 1", () => {
-    expect(1).toBe(1);
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../../../app");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Connect to the database before each test
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+});
+
+// Closing database connection after each test
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe("GET /users", () => {
+  it("should return all users", async () => {
+    const res = await request(app).get("/users");
+    expect(res.statusCode).toBe(200);
   });
 });
 
-// const mongoose = require("mongoose");
-// const request = require("supertest");
-// const app = require("../../../app");
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// // jest.setTimeout(60000);
-
-// // Connect to the database before each test
-// beforeEach(async () => {
-//   await mongoose.connect(process.env.MONGODB_URI);
-// });
-
-// // Closing database connection after each test
-// afterEach(async () => {
-//   await mongoose.connection.close();
-// });
-
-// describe("GET /users", () => {
-//   it("should return all users", async () => {
-//     const res = await request(app).get("/users");
-//     expect(res.statusCode).toBe(200);
-//   });
-// });
-
-// describe("GET id", () => {
-//   it("should return a single book by id", async () => {
-//     const res = await request(app).get("/books/6376b660b06610cce064cc31");
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body.title).toBe("Pride and Prejudice");
-//   });
-// });
-
-// describe("GET title", () => {
-//   it("should return a single book by title", async () => {
-//     const res = await request(app).get("/books/title/Persuasion");
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].title).toBe("Persuasion");
-//   });
-// });
-
-// describe("GET author", () => {
-//   it("should return a single book by author", async () => {
-//     const res = await request(app).get("/books/author/Jane%20Austen");
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].author).toBe("Jane Austen");
-//   });
-// });
-
-// describe("GET genre", () => {
-//   it("should return a single book by genre", async () => {
-//     const res = await request(app).get("/books/genre/romance");
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].genre).toBe("ROMANCE");
-//   });
-// });
+describe("GET id", () => {
+  it("should return a single user by id", async () => {
+    const res = await request(app).get("/users/6374fe62191e03ef27dec2f5");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.username).toBe("Timmy");
+  });
+});
 
 // describe("POST book", () => {
 //   it("should create a new book", async () => {
@@ -77,55 +45,23 @@ describe("Sanity test", () => {
 //   });
 // });
 
-// describe("PUT book", () => {
-//   it("should update a book", async () => {
-//     const res = await request(app).put("/books/6376b660b06610cce064cc31").send({
-//       title: "Pride and Prejudice",
-//       author: "Jane Austen",
-//       description:
-//         "There is pride and there is prejudice and also Darcy is in it.",
-//       genre: "history romance",
-//       ownerId: "6374fe62191e03ef27dec2f5",
-//       availability: "false",
-//       location: "300 Bruce Hill Rd",
-//     });
-//     expect(res.statusCode).toBe(204);
-//   });
-// });
+describe("PUT book", () => {
+  it("should update a user", async () => {
+    const res = await request(app).put("/users/63765476893de1c9d6db4526").send({
+      username: "HarryBobby",
+      firstName: "Harry",
+      lastName: "Robert",
+      streetAddress: "230 Bruce Hill Rd",
+      email: "harry4@example.com",
+      phoneNum: "207-444-6666",
+    });
+    expect(res.statusCode).toBe(204);
+  });
+});
 
-// // BEFORE EVERY TEST RUN: update with existing Mansfield Park ID
 // describe("DELETE book by id", () => {
 //   it("should delete a single book by id", async () => {
 //     const res = await request(app).delete("/books/6384d833e8a4ead8fd712cbe");
 //     expect(res.statusCode).toBe(200);
-//   });
-// });
-
-// const { MongoClient } = require("mongodb");
-
-// describe("insert", () => {
-//   let connection;
-//   let db;
-
-//   beforeAll(async () => {
-//     connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     db = await connection.db(globalThis.__MONGO_DB_NAME__);
-//   });
-
-//   afterAll(async () => {
-//     await connection.close();
-//   });
-
-//   it("should insert a doc into collection", async () => {
-//     const users = db.collection("users");
-
-//     const mockUser = { _id: "some-user-id", name: "John" };
-//     await users.insertOne(mockUser);
-
-//     const insertedUser = await users.findOne({ _id: "some-user-id" });
-//     expect(insertedUser).toEqual(mockUser);
 //   });
 // });
