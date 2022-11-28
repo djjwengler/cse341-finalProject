f8ab28f2d75df11c1ddef2836ebebb27a9381e75
djjwenger <djjwenger@hotmail.com>
2022-11-28 11:01:53 -0500

    working book route tests
    Co-authored-by: OneDeMillion <OneDeMillion@users.noreply.github.com>

--- src/controllers/tests/books.test.js
+++ src/controllers/tests/books.test.js
@@ -1,70 +1,90 @@
-const {
-  Types: { ObjectId },
-} = require("mongoose");
+// const {
+//   Types: { ObjectId },
+// } = require("mongoose");
+const mongoose = require("mongoose");
 const mockingoose = require("mockingoose");
 const BooksModel = require("../../models/book.js");
 const db = require("../../models");
 const BookModel = db.book;
 const booksController = require("../books");
 const TestResponse = require("./testResponse");
+const request = require("supertest");
+const { getOneById } = require("../books");
+const app = require("../../../app");
 
-const getAll = booksController.getAll;
+// const getOneById = booksController.getOneById;
 
 jest.setTimeout(60000);
 
-describe("Books service", () => {
-  describe("getAll", () => {
-    test("should return the list of books", async () => {
-      const _book = {
-        title: "Book 1",
-        author: "Author Name",
-        description: "Book description",
-        genre: "Book genre",
-        ownerId: "6374fe62191e03ef27dec2f5",
-        availability: true,
-        location: "Book location",
-      };
+// describe("getAll", () => {
+//   test("should return the list of books", async () => {
+//     const _book = {
+//       _id: ObjectId("6376b660b06610cce064cc31"),
+//       title: "Book 1",
+//       author: "Author Name",
+//       description: "Book description",
+//       genre: "Book genre",
+//       ownerId: "6374fe62191e03ef27dec2f5",
+//       availability: true,
+//       location: "Book location",
+//     };
 
-      mockingoose(BookModel).toReturn(_book, "find");
+//     mockingoose(BookModel).toReturn(_book, "findOne");
 
-      // const req = {
-      //     params: {}
-      // }
+//     const req = {
+//       params: { _id: "6376b660b06610cce064cc31" },
+//     };
 
-      const res = new TestResponse();
+//     const res = new TestResponse();
 
-      await getAll;
-      expect(res.statusCode).toBe(200);
-      expect(res.data).toEqual(_book);
+//     await getOneById(req, res);
+//     expect(res.statusCode).toBe(200);
+//     expect(res.data).toEqual(_book);
 
-      //   mockingoose(BookModel).toReturn(
-      //     [
-      //       {
-      //         title: "Book 1",
-      //         author: "Author Name",
-      //         description: "Book description",
-      //         genre: "Book genre",
-      //         ownerId: "6374fe62191e03ef27dec2f5",
-      //         availability: true,
-      //         location: "Book location",
-      //       },
-      //       {
-      //         title: "Book 2",
-      //         author: "Author2 Name",
-      //         description: "Book2 description",
-      //         genre: "Book2 genre",
-      //         ownerId: "6374fe62191e03ef27dec2f5",
-      //         availability: true,
-      //         location: "Book2 location",
-      //       },
-      //     ],
-      //     "find"
-      //   );
-      //   const results = await booksController.getAll;
-      //   expect(results[0].title).toBe("Book 1");
-    });
+// Connect to the database before each test
+beforeEach(async () => {
+  await mongoose.connect(process.env._MONGODB_URI);
+});
+
+// Closing database connection after each test
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe("GETall books", () => {
+  it("should return all books", async () => {
+    const res = await request(app).get("/books");
+    expect(res.statusCode).toBe(200);
   });
 });
+
+// mockingoose(BookModel).toReturn(
+//   [
+//     {
+//       title: "Book 1",
+//       author: "Author Name",
+//       description: "Book description",
+//       genre: "Book genre",
+//       ownerId: "6374fe62191e03ef27dec2f5",
+//       availability: true,
+//       location: "Book location",
+//     },
+//     {
+//       title: "Book 2",
+//       author: "Author2 Name",
+//       description: "Book2 description",
+//       genre: "Book2 genre",
+//       ownerId: "6374fe62191e03ef27dec2f5",
+//       availability: true,
+//       location: "Book2 location",
+//     },
+//   ],
+//   "find"
+// );
+// const results = await booksController.getAll;
+// expect(results[0].title).toBe("Book 1");
+//   });
+// });
 
 // describe("Books service", () => {
 //   describe("getAll", () => {
--- src/routes/tests/books.test.js
+++ src/routes/tests/books.test.js
@@ -1,5 +1,96 @@
-describe("Sanity test", () => {
-  test("1 should equal 1", () => {
-    expect(1).toBe(1);
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../../../app");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// jest.setTimeout(60000);
+
+// Connect to the database before each test
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+});
+
+// Closing database connection after each test
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe("GETall books", () => {
+  it("should return all books", async () => {
+    const res = await request(app).get("/books");
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET id", () => {
+  it("should return a single book by id", async () => {
+    const res = await request(app).get("/books/6376b660b06610cce064cc31");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe("Pride and Prejudice");
+  });
+});
+
+describe("GET title", () => {
+  it("should return a single book by title", async () => {
+    const res = await request(app).get("/books/title/Persuasion");
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].title).toBe("Persuasion");
+  });
+});
+
+describe("GET author", () => {
+  it("should return a single book by author", async () => {
+    const res = await request(app).get("/books/author/Jane%20Austen");
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].author).toBe("Jane Austen");
+  });
+});
+
+describe("GET genre", () => {
+  it("should return a single book by genre", async () => {
+    const res = await request(app).get("/books/genre/romance");
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].genre).toBe("ROMANCE");
+  });
+});
+
+describe("POST book", () => {
+  it("should create a new book", async () => {
+    const res = await await request(app).post("/books").send({
+      title: "Mansfield Park",
+      author: "Jane Austen",
+      description: "Sadness and then happiness",
+      genre: "romance",
+      ownerId: "6374fe62191e03ef27dec2f5",
+      availability: "false",
+      location: "300 Bruce Hill Rd",
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.body.title).toBe("Mansfield Park");
+  });
+});
+
+describe("PUT book", () => {
+  it("should update a book", async () => {
+    const res = await request(app).put("/books/6376b660b06610cce064cc31").send({
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      description:
+        "There is pride and there is prejudice and also Darcy is in it.",
+      genre: "history romance",
+      ownerId: "6374fe62191e03ef27dec2f5",
+      availability: "false",
+      location: "300 Bruce Hill Rd",
+    });
+    expect(res.statusCode).toBe(204);
+  });
+});
+
+// BEFORE EVERY TEST RUN: update with existing Mansfield Park ID
+describe("DELETE book by id", () => {
+  it("should delete a single book by id", async () => {
+    const res = await request(app).delete("/books/6384d833e8a4ead8fd712cbe");
+    expect(res.statusCode).toBe(200);
   });
 });
